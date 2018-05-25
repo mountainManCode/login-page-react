03b97aff451a8e63d48c5a4d8a14f292c5c7e132
Aaron <aa.griff9@gmail.com>
2018-05-25 14:08:14 -0700

    Added and rendering a login container.

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../../styles/css/login.css';
+
+const Login = () => (
+    <div className="login-container">This is the login page</div>
+);
+
+export default Login;
---
+++ src/containers/Login/LoginContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Login from './Login';
+
+class LoginContainer extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return <Login />;
+    }
+}
+
+export default LoginContainer;
